becf5ce520f995a14e9709258f28b678221e5874
Gidi Rubin <gidierubin@gmail.com>
2023-07-26 12:26:42 +0300

    Update to allow cors

--- server/index.js
+++ server/index.js
@@ -7,15 +7,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 var value = null;
 
-
-
-
 app.get("/set/:id", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.json({ message: "Value set: "+req.params.id });
   this.value = req.params.id;
 });
 
 app.get("/get", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.json({ message: this.value });
 });
 
